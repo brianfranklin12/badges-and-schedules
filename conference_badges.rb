38add49ae19f9a319e375c6967f77c8ff01e3d2b
Brian Franklin <brianfranklinmusic@gmail.com>
2020-09-29 13:58:39 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge_messages = []
+    array.each do |name|
+        badge_messages.push("Hello, my name is #{name}.")
+    end
+    return badge_messages
+end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    speakers.each_with_index do |speaker, index|
+        room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+    end
+    return room_assignments
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each { |speaker| puts speaker}
+    assign_rooms(speakers).each { |speaker| puts speaker}
+end
